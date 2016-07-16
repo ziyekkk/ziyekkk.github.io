8ac69655a23cdc05a28a0fe3af55e612bd995236
ziyekkk <1024465429@qq.com>
2016-07-16 14:24:18 +0800

    Add files via upload
    上下左右功能完成

--- 2048js/game.js
+++ 2048js/game.js
@@ -7,11 +7,23 @@ $(document).keydown(function(event){
 			}
 			break;
 		case 38://up
+		if(moveUp()){
+				generateOneNumber();
+			
+			}
 			break;
 		case 39://right
-		
+		if(moveRight()){
+				generateOneNumber();
+			
+			}
+			
 			break;
 		case 40://down
+		if(moveDown()){
+				generateOneNumber();
+			
+			}
 			break;
 		default:
 			break;
@@ -44,4 +56,84 @@ function moveLeft(){
 
     return true;
 }
+function moveRight(){
+	if(!canMoveRight(board)){
+		return false;
+	}
+	//moveRight
+	for(var i=3;i>=0;i--){
+		for(var j=2;j>=0;j--){
+			if(board[i][j]!=0){
+				for(var k=3;k>j;k--){
+					if(board[i][k]==0&&noBlockHorizontalCol(i,j,k,board)){
+						showMoveAnimation(i,j,i,k);
+						board[i][k]=board[i][j];
+						board[i][j]=0;
+					}else if(board[i][k]==board[i][j]&&noBlockHorizontalCol(i, j, k, board)){
+						showMoveAnimation(i,j,i,k);
+						board[i][k]+=board[i][j];
+						board[i][j]=0;
+						
+					}
+				}
+			}
+		}
+	}
+	setTimeout("updateBoardView();",200);
 
+    return true;
+}
+function moveUp(){
+	if(!canMoveUp(board)){
+		return false;
+	}
+	//moveUp
+	for(var j=0;j<4;j++){
+		for(var i=1;i<4;i++){
+			if(board[i][j]!=0){
+				for(var k=0;k<i;k++){
+					if(board[k][j]==0&&noBlockHorizontalColud(j,k,i,board)){
+						showMoveAnimation(i,j,k,j);
+						board[k][j]=board[i][j];
+						board[i][j]=0;
+					}else if(board[k][j]==board[i][j]&&noBlockHorizontalColud(j,k,i,board)){
+						showMoveAnimation(i,j,i,k);
+						board[k][j]+=board[i][j];
+						board[i][j]=0;
+						
+					}
+				}
+			}
+		}
+	}
+	setTimeout("updateBoardView();",200);
+
+    return true;
+}
+function moveDown(){
+	if(!canMoveDown(board)){
+		return false;
+	}
+	//moveUp
+	for(var j=0;j<4;j++){
+		for(var i=2;i>=0;i--){
+			if(board[i][j]!=0){
+				for(var k=3;k>i;k--){
+					if(board[k][j]==0&&noBlockHorizontalColud(j,k,i,board)){
+						showMoveAnimation(i,j,k,j);
+						board[k][j]=board[i][j];
+						board[i][j]=0;
+					}else if(board[k][j]==board[i][j]&&noBlockHorizontalColud(j,k,i,board)){
+						showMoveAnimation(i,j,i,k);
+						board[k][j]+=board[i][j];
+						board[i][j]=0;
+						
+					}
+				}
+			}
+		}
+	}
+	setTimeout("updateBoardView();",200);
+
+    return true;
+}
