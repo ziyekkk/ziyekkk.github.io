f1151dcae625d787f921ea5bf858cb4c39caf75e
ziyekkk <1024465429@qq.com>
2016-08-19 15:01:35 +0800

    test

--- jdsame/js/patch.js
+++ jdsame/js/patch.js
@@ -12,7 +12,7 @@ function assembleMenu(UlParentNode){
 	for(var i=0;i<theUlChilds.length;i++){
 		if(theUlChilds[i].tagName=="LI"){
 			var theLiNode=theUlChilds[i];
-			setMouseActions(theLiNode,menu1);
+			setMouseActions(theLiNode);
 		}
 	}
 }
@@ -20,12 +20,11 @@ function setMouseActions(node,node1){
 	node.onmouseover=function(){
 		this.style.backgroundColor="#f7f7f7";
 		this.getElementsByTagName("a")[0].style.color="#b61d1d";
-		node1.style.display="block";
-		node1.style.zIndex="100";
+		
 	}
 	node.onmouseout=function(){
 		this.style.backgroundColor="#c81623";
 		this.getElementsByTagName("a")[0].style.color="#fff";
-		node1.style.display="none";
+		
 	}
 }
